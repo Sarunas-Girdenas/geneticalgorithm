9c89805ded90788049c3accbf2215cff33c99d86
Sarunas Girdenas <sarunas@Sarunass-MacBook-Pro.local>
2019-10-07 17:19:28 +0100

    r

--- chromosome.py
+++ chromosome.py
@@ -15,5 +15,11 @@ class Chromosome():
         return self.seed
 
     def cross(self, another_chromosome: "Chromosome", crossover_index: int) -> Tuple["Chromosome", "Chromosome"]:
-            
-        return (another_chromosome, Chromosome("0"))
+
+        if crossover_index == 1:
+            first_seed = f"{self.value[0]}{another_chromosome.value[1]}"
+            second_seed = f"{another_chromosome.value[0]}{self.value[1]}"
+            return (Chromosome(first_seed), Chromosome(second_seed))
+        
+
+        return (another_chromosome, self)
--- test_genetic_algorithm.py
+++ test_genetic_algorithm.py
@@ -5,37 +5,43 @@ from chromosome import Chromosome
 class Testalgo(unittest.TestCase):
 
     @parameterized.expand([
-        ["0","0",0,"0"],
-        ["0","1",0,"1"],
-        ["1","0",0,"0"],
-        ["1","3",0,"3"]
-
-        ])
-
-    def test_crossover(self, first_chromosome_seed: str, second_chromosome_seed: str,
-                       crossover_index: int, first_chromosome_crossed_seed: str):
-
-        first_chromosome = Chromosome(first_chromosome_seed)
-        second_chromosome = Chromosome(second_chromosome_seed)
-
-        first_chromosome_crossed = first_chromosome.cross(second_chromosome, crossover_index)[0]
-
-        self.assertEqual(first_chromosome_crossed, Chromosome(first_chromosome_crossed_seed),
-            f"crossed: {first_chromosome_crossed.value}, seed: {first_chromosome_crossed_seed}")
-
-
-    @parameterized.expand([
         ["0","0",0,"0","0"],
-        ["0","1",0,"1","0"]
-
+        ["0","1",0,"1","0"],
+        ["1","1",0,"1","1"]
     ])
-    def test_crossover_returning_two(self,first_chromosome_seed: str, second_chromosome_seed: str, crossover_index: int, first_chromosome_crossed_seed: str, second_chromosome_crossed_seed: str):
+
+    def test_crossover_at_index_0(self, first_chromosome_seed: str, second_chromosome_seed: str,
+                                  crossover_index: int, first_chromosome_crossed_seed: str,
+                                  second_chromosome_crossed_seed: str):
+
         first_chromosome = Chromosome(first_chromosome_seed)
         second_chromosome = Chromosome(second_chromosome_seed)
 
         first_chromosome_crossed = first_chromosome.cross(second_chromosome, crossover_index)[0]
         second_chromosome_crossed = first_chromosome.cross(second_chromosome, crossover_index)[1]
 
+        self.assertEqual(first_chromosome_crossed, Chromosome(first_chromosome_crossed_seed),
+            f"crossed: {first_chromosome_crossed.value}, seed: {first_chromosome_crossed_seed}")
+
+        self.assertEqual(second_chromosome_crossed, Chromosome(second_chromosome_crossed_seed),
+            f"crossed: {second_chromosome_crossed.value}, seed: {second_chromosome_crossed_seed}")
+    
+
+    @parameterized.expand([
+        ["11", "00", 1, "10", "01"],
+        ["10", "01", 1, "11", "00"],
+        ["01", "01", 1, "01", "01"]
+    ])
+
+    def test_crossover_at_index_1(self, first_chromosome_seed: str, second_chromosome_seed: str,
+                                  crossover_index: int, first_chromosome_crossed_seed: str,
+                                  second_chromosome_crossed_seed: str):
+
+        first_chromosome = Chromosome(first_chromosome_seed)
+        second_chromosome = Chromosome(second_chromosome_seed)
+
+        first_chromosome_crossed = first_chromosome.cross(second_chromosome, crossover_index)[0]
+        second_chromosome_crossed = first_chromosome.cross(second_chromosome, crossover_index)[1]
 
         self.assertEqual(first_chromosome_crossed, Chromosome(first_chromosome_crossed_seed),
             f"crossed: {first_chromosome_crossed.value}, seed: {first_chromosome_crossed_seed}")
