37651c1c75593854df2b007803038a68848c1606
Sarunas Girdenas <sgirdenas@gmail.com>
2019-10-29 15:13:19 +0000

    r

--- chromosome.py
+++ chromosome.py
@@ -20,25 +20,11 @@ class Chromosome:
     def mutate(self, injected_random_number:"List") -> "Chromosome":
         threshold = 0.5
 
-        if len(injected_random_number) == 2:
-            chromosome_seed = ""
-            for number, chromosome_value in zip(injected_random_number, self.seed):
-                chromosome_seed += Chromosome.mutate_element(number, chromosome_value)
-            return Chromosome(chromosome_seed)
-
-
-        if injected_random_number[0] > threshold:
-            chromosome_value = ["0"]
-            chromosome_seed = ""
-            for number, chromosome_value in zip(injected_random_number, chromosome_value):
-                chromosome_seed += Chromosome.mutate_element(number, chromosome_value)
-            return Chromosome(chromosome_seed)
+        chromosome_seed = ""
+        for number, chromosome_value in zip(injected_random_number, self.seed):
+            chromosome_seed += Chromosome.mutate_element(number, chromosome_value)
+        return Chromosome(chromosome_seed)
             
-            
-        
-        if injected_random_number[0] <= threshold:
-            return Chromosome("0")
-
     @staticmethod
     def mutate_element(random_number, chromosome_value):
         threshold = 0.5
